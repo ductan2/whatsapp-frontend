60ff794eb9bcebff4dec4e64823ec2602f17987a
ductan2 <ndt11102003@gmail.com>
2023-11-21 10:23:04 +0700

    deloy

--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -1,29 +1,40 @@
-import { useState } from "react"
-import Notifications from "../notifications/Notifications"
-import SideBarHeader from "./SideBarHeader"
-import Search from "../search/Search"
-import Conversation from "../conversation/Conversation"
-import { IUser } from "../../types/user.type"
-import SearchResult from "../search/SearchResult"
-
+import { useState } from "react";
+import Notifications from "../notifications/Notifications";
+import SideBarHeader from "./SideBarHeader";
+import Search from "../search/Search";
+import Conversation from "../conversation/Conversation";
+import { IUser } from "../../types/user.type";
+import SearchResult from "../search/SearchResult";
 
 const SideBar = () => {
-
-   const [searchResult, setSearchResult] = useState<IUser[]>([])
-   const [search, setSearch] = useState<string>("")
-   const [show, setShow] = useState<boolean>(false)
+   const [searchResult, setSearchResult] = useState<IUser[]>([]);
+   const [search, setSearch] = useState<string>("");
+   const [show, setShow] = useState<boolean>(false);
    return (
       <div className="sm:w-[50%] lg:w-[40%] h-full select-none">
          <SideBarHeader />
-         <Notifications />
-         <Search search={search} show={show} setShow={setShow} setSearch={setSearch} searchResult={searchResult} setSearchResult={setSearchResult} />
-         {!show ?
-            <Conversation /> :
-            <SearchResult setSearchResult={setSearchResult} search={search} data={searchResult} />
-         }
-
+         <>
+            <Notifications />
+            <Search
+               search={search}
+               show={show}
+               setShow={setShow}
+               setSearch={setSearch}
+               searchResult={searchResult}
+               setSearchResult={setSearchResult}
+            />
+            {!show ? (
+               <Conversation />
+            ) : (
+               <SearchResult
+                  setSearchResult={setSearchResult}
+                  search={search}
+                  data={searchResult}
+               />
+            )}
+         </>
       </div>
-   )
-}
+   );
+};
 
-export default SideBar
+export default SideBar;
